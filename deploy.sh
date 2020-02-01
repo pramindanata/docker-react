a40fcd97dfd635116f41562592d01aff63865b8a
pramindanata <pramindanata.eksa@gmail.com>
2020-02-01 21:03:20 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker build -t pramindanata/docker-react -f Dockerfile.prod .
-docker stop web
-docker run --name web -d -p 80:80 pramindanata/docker-react
+docker stop react-web
+docker run --name react-web -d -p 80:80 pramindanata/docker-react
+# docker rmi $(docker images -f "dangling=true" -q) --force

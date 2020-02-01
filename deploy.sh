8900d64745e1d5bfa3929ae99bcfb7c103cbd737
pramindanata <pramindanata.eksa@gmail.com>
2020-02-01 21:56:41 +0800

    update travis and deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-echo "# Build Image"
+green=`tput setaf 3`
+reset=`tput sgr0`
+
+echo "${green}# Build Image${reset}"
 docker build -t pramindanata/docker-react -f Dockerfile.prod .
 
-echo "# Stop Previous Container"
-docker stop react-web
+echo "${green}# Stop & Remove Previous Container${reset}"
+docker rm -f react-web
 
-echo "# Run Container"
+echo "${green}# Run Container${reset}"
 docker run --name react-web -d -p 80:80 pramindanata/docker-react
 # docker rmi $(docker images -f "dangling=true" -q) --force
